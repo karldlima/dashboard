09432314e60e905e68f7efd4fe8a2140254bfed7
Kito <kito@Buyers-MacBook-Air.local>
2023-02-01 21:34:40 +1100

    themes and colors

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,35 +1,45 @@
-import React from 'react'
-import {Link, NavLink} from 'react-router-dom'
-import {SiShopware} from 'react-icons/si'
-import {MdOutlineCancel} from 'react-icons/md'
-import {TooltipComponent} from '@syncfusion/ej2-react-popups'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { SiShopware } from "react-icons/si";
+import { MdOutlineCancel } from "react-icons/md";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
-import { links } from '../data/dummy'
+import { links } from "../data/dummy";
 
-import { useStateContext } from '../contexts/ContextProvider'
-
+import { useStateContext } from "../contexts/ContextProvider";
 
 const SideBar = () => {
-  const {activeMenu, setActiveMenu, screenSize} = useStateContext()
+  const { activeMenu, setActiveMenu, screenSize, currentColor } =
+    useStateContext();
 
   const handleCloseSideBar = () => {
-    if(activeMenu && screenSize <= 900) {
-      setActiveMenu(false)
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false);
     }
-  }
+  };
 
-  const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2'
-  const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2'
+  const activeLink =
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
+  const normalLink =
+    "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2";
   return (
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10">
       {activeMenu && (
         <>
           <div className="flex justify-between items-center">
-            <Link to="/" onClick={handleCloseSideBar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900">
+            <Link
+              to="/"
+              onClick={handleCloseSideBar}
+              className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
+            >
               <SiShopware /> <span>Shoppy</span>
             </Link>
             <TooltipComponent content="Menu" position="BottomCenter">
-              <button type="button" onClick={() => setActiveMenu((prev) => !prev)} className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden">
+              <button
+                type="button"
+                onClick={() => setActiveMenu((prev) => !prev)}
+                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+              >
                 <MdOutlineCancel />
               </button>
             </TooltipComponent>
@@ -37,14 +47,22 @@ const SideBar = () => {
           <div className="mt-10">
             {links.map((item) => (
               <div key={item.title}>
-                <p className="text-gray-400 m-3 mt-4 uppercase">
-                  {item.title}
-                </p>
+                <p className="text-gray-400 m-3 mt-4 uppercase">{item.title}</p>
                 {item.links.map((Link) => (
-                   <NavLink to={`/${Link.name}`} key={Link.name} onClick={handleCloseSideBar} className={({isActive}) => isActive ? activeLink : normalLink}>
+                  <NavLink
+                    to={`/${Link.name}`}
+                    key={Link.name}
+                    onClick={handleCloseSideBar}
+                    style={({ isActive }) => ({
+                      backgroundColor: isActive ? currentColor : "",
+                    })}
+                    className={({ isActive }) =>
+                      isActive ? activeLink : normalLink
+                    }
+                  >
                     {Link.icon}
                     <span className="capitalize">{Link.name}</span>
-                   </NavLink>
+                  </NavLink>
                 ))}
               </div>
             ))}
@@ -52,7 +70,7 @@ const SideBar = () => {
         </>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default SideBar
+export default SideBar;
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,50 +1,57 @@
-import React, {createContext, useContext, useState} from 'react'
+import React, { createContext, useContext, useState } from "react";
 
-const StateContext = createContext()
+const StateContext = createContext();
 
 const initialState = {
-    chat: false,
-    cart: false,
-    userProfile: false,
-    notification: false,
-}
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
+};
 
-export const ContextProvider = ({children}) => {
-    const [activeMenu, setActiveMenu] = useState(true)
-    const [isClicked, setIsClicked] = useState(initialState)
-    const [screenSize, setScreenSize] = useState(undefined)
-    const [currentColor, setCurrentColor] = useState('#03c9d7')
-    const [currentMode, setCurrentMode] = useState('Light')
-    const [ThemeSettings, setThemeSettings] = useState(false)
+export const ContextProvider = ({ children }) => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+  const [screenSize, setScreenSize] = useState(undefined);
+  const [currentColor, setCurrentColor] = useState("#03c9d7");
+  const [currentMode, setCurrentMode] = useState("Light");
+  const [themeSettings, setThemeSettings] = useState(false);
 
-    const setMode = (e) => {
-        setCurrentMode(e.target.value)
-        localStorage.setItem('themeMode', e.target.value)
-    }
+  const setMode = (e) => {
+    setCurrentMode(e.target.value);
+    localStorage.setItem("themeMode", e.target.value);
+    setThemeSettings(false);
+  };
 
-    
-    const setColor = (e) => {
-        setCurrentColor(e.target.value)
-        localStorage.setItem('colorMode', e.target.value)
-    }
+  const setColor = (col) => {
+    setCurrentColor(col);
+    localStorage.setItem("colorMode", col);
+    setThemeSettings(false);
+  };
 
-    const handleClick = (clicked) => setIsClicked({...initialState, [clicked]: true })
-    return (
-        <StateContext.Provider
-            value={{
-                activeMenu,
-                setActiveMenu,
-                isClicked, setIsClicked,
-                handleClick,
-                screenSize, setScreenSize,
-                currentColor, currentMode,
-                setCurrentColor, setCurrentMode,
-                ThemeSettings, setThemeSettings
-            }}
-        >
-            {children}
-        </StateContext.Provider>
-    )
-}
+  const handleClick = (clicked) =>
+    setIsClicked({ ...initialState, [clicked]: true });
+  return (
+    <StateContext.Provider
+      value={{
+        activeMenu,
+        setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+        screenSize,
+        setScreenSize,
+        currentColor,
+        currentMode,
+        setMode,
+        setColor,
+        themeSettings,
+        setThemeSettings,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
 
-export const useStateContext = () => useContext(StateContext)
+export const useStateContext = () => useContext(StateContext);
